04c9a73165ed05f298000c20bf1e6c994d716060
Akshat Vyas <akshatvyas110@gmail.com>
2024-12-15 13:22:29 +0530

    Frist commit in master

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! This is a sample code for practicing concepts of git and github.");
+    }
+}
