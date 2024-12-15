5a13d52190174bf51283e3570c8d25c43b9d160d
Akshat Vyas <akshatvyas110@gmail.com>
2024-12-15 13:23:28 +0530

    Frist commit in dev branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! This is a sample code for practicing concepts of git and github.");
+        System.out.println("This line was added in the dev branch");
     }
 }
